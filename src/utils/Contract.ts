ebf20143f9d2e17a4a86d94916157f34b91a6e3c
Rahul Roy <rroy1836@gmail.com>
2026-01-14 03:41:36 +0530

    Added Contracts

---
+++ src/utils/Contract.ts
@@ -0,0 +1,27 @@
+import { defineChain } from 'viem'
+
+export const mantleSepolia = defineChain({
+  id: 5003,
+  name: 'Mantle Sepolia Testnet',
+  network: 'mantle-sepolia',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Mantle',
+    symbol: 'MNT',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.sepolia.mantle.xyz'],
+    },
+    public: {
+      http: ['https://rpc.sepolia.mantle.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Mantle Sepolia Explorer',
+      url: 'https://explorer.sepolia.mantle.xyz',
+    },
+  },
+  testnet: true,
+})
